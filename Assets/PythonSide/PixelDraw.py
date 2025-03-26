9473f0842c6af02dfd639b33131361929e853517
kdix-23-0240 <2312110240g@kindai.ac.jp>
2025-03-26 14:46:13 +0900

    色の順番入れ替え

--- Assets/PythonSide/PixelDraw.py
+++ Assets/PythonSide/PixelDraw.py
@@ -3,58 +3,57 @@
 if __name__ == "__main__":
     # 色の定義
     RESET = "\033[0m"      # リセット
+    WHITE = "\033[37m"     # 白
     BLACK = "\033[30m"     # 黒
     RED = "\033[31m"       # 赤
+    BLUE = "\033[34m"      # 青
     GREEN = "\033[32m"     # 緑
     YELLOW = "\033[33m"    # 黄
-    BLUE = "\033[34m"      # 青
-    WHITE = "\033[37m"     # 白
+    PINK = "\033[38;5;206m"  # ピンク（ANSI 256色モード）
+    LIGHT_BLUE = "\033[36m"  # 水色
+    LIGHT_GREEN = "\033[38;5;190m"  # 黄緑（ANSI 256色モード）
     ORANGE = "\033[38;5;214m"  # オレンジ（ANSI 256色モード）
     PURPLE = "\033[35m"    # 紫
-    LIGHT_BLUE = "\033[36m"  # 水色
-    PINK = "\033[38;5;206m"  # ピンク（ANSI 256色モード）
-    LIGHT_GREEN = "\033[38;5;190m"  # 黄緑（ANSI 256色モード）
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     # 色付きの文字を表示
-    a = RED + "■" + RESET
-    b = GREEN + "■" + RESET
-    c = YELLOW + "■" + RESET
+    a = WHITE + "■" + RESET
+    b = BLACK + "■" + RESET
+    c = RED + "■" + RESET
     d = BLUE + "■" + RESET
-    e = WHITE + "■" + RESET
-    f = ORANGE + "■" + RESET
-    g = PURPLE + "■" + RESET
+    e = GREEN + "■" + RESET
+    f = YELLOW + "■" + RESET
+    g = PINK + "■" + RESET
     h = LIGHT_BLUE + "■" + RESET
-    i = PINK + "■" + RESET
-    j = LIGHT_GREEN + "■" + RESET
-
-    # ---------------------ここから---------------------------
+    i = LIGHT_GREEN + "■" + RESET
+    j = ORANGE + "■" + RESET
+    k = PURPLE + "■" + RESET
     
     # ピクセルアート格納用の二次元配列を定義
-    list_width = 13
-    list_height = 20
-    pixel_art = [[e]*list_width]*list_height # 12x18 の白いピクセルアート
+    list_width = 5 
+    list_height = 5
+    pixel_art = [[e]*list_width]*list_height
 
     #リスト化
-    pixel_art[0] = [a,a,c,c,c,c,a,a,g,g,g,g,g]
-    pixel_art[1] = [b,c,b,b,b,b,c,b,g,g,g,g,g]
-    pixel_art[2] = [c,c,c,c,c,c,c,c,g,g,g,g,g]
-    pixel_art[3] = [d,d,d,d,d,d,d,d,g,g,g,g,g]
-    pixel_art[4] = [e,e,f,e,e,f,e,e,g,g,g,g,g]
-    pixel_art[5] = [f,f,f,i,i,f,f,f,g,g,g,g,g]
-    pixel_art[6] = [g,g,g,j,j,g,g,g,g,g,g,g,g]
-    pixel_art[7] = [g,g,g,j,j,g,g,g,g,g,g,g,g]
-    pixel_art[8] = [g,g,g,j,j,g,g,g,g,g,g,g,g]
-    pixel_art[9] = [g,g,g,j,j,g,g,g,g,g,g,g,g]
-    pixel_art[10] = [g,g,g,j,j,g,g,g,g,g,g,g,g]
-    pixel_art[11] = [g,g,g,j,j,g,g,g,g,g,g,g,g]
-    pixel_art[12] = [g,g,g,j,j,g,g,g,g,g,g,g,g]
-    pixel_art[13] = [g,g,g,j,j,g,g,g,g,g,g,g,g]
-    pixel_art[14] = [g,g,g,j,j,g,g,g,g,g,g,g,g]
-    pixel_art[15] = [g,g,g,j,j,g,g,g,g,g,g,g,g]
-    pixel_art[16] = [g,g,g,j,j,g,g,g,g,g,g,g,g]
-    pixel_art[17] = [g,g,g,j,j,g,g,g,g,g,g,g,g]
-    pixel_art[18] = [g,g,g,j,j,g,g,g,g,g,g,g,g]
-    pixel_art[19] = [a,a,a,j,j,a,a,a,a,a,a,a,a]
+    pixel_art = [   [f,f,f,f,f],
+                    [f,b,f,b,f],
+                    [f,b,f,b,f],
+                    [g,f,f,f,g],
+                    [f,b,b,b,f]
+                ]
 
     # ピクセルアートを表示
     for i in range(len(pixel_art)):
